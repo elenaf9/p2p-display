f8f9d3fb70323810c334b30aa99401fccfe49d1a
BrunoStendal <bruno.r3ad@gmail.com>
2022-06-27 00:35:48 +0200

    feat(display): toDisplay now operates with markdown table format

--- display/examples/EPD_4in2_test.c
+++ display/examples/EPD_4in2_test.c
@@ -1,42 +1,34 @@
-/*****************************************************************************
-* | File      	:   EPD_4in2_test.c
-* | Author      :   Waveshare team
-* | Function    :   4.2inch e-paper test demo
-* | Info        :
-*----------------
-* |	This version:   V1.0
-* | Date        :   2019-06-13
-* | Info        :
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documnetation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to  whom the Software is
-# furished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-******************************************************************************/
 #include "EPD_Test.h"
 #include "EPD_4in2.h"
 #include <string.h>
 #include <time.h> 
+#include <stdbool.h>
 
 UBYTE *BlackImage;
 
-int EPD_toDisplay(){
+int sizeLines;
+int sizeRows;
+int widthRows[];
 
-    printf("EPD_4IN2_test Demo\r\n");
+int drawnToX;
+int borderOffset = 10;
+
+int EPD_toDisplay(int sizeL, int sizeR,int percWidth[]){
+
+    int widthRows[sizeR];
+    for(int i = 0; i < sizeR; i++){
+        widthRows[i] = percWidth[i];
+    }
+
+
+    sizeLines = sizeL;
+    sizeRows = sizeR;
+
+    for(int a = 0; a < sizeR; a++){
+        printf(" percWidth: %d %d | ",a,widthRows[a]);
+    }
+    printf("\n");
+ 
     if(DEV_Module_Init()!=0){
         return -1;
     }
@@ -49,56 +41,88 @@ int EPD_toDisplay(){
         printf("Failed to apply for black memory...\r\n");
         return -1;
     }
-    printf("Paint_NewImage\r\n");
+
     Paint_NewImage(BlackImage, EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, 270, WHITE);
-
-    printf("e-Paper Init and Clear...\r\n");
     EPD_4IN2_Init_Fast();
-
 
     EPD_4IN2_Clear();
     Paint_SelectImage(BlackImage);
     Paint_Clear(WHITE);
 
+    drawnToX = 0;
+
     return 0;
 }
 
+int calcTextPos(int index, int width, char *input, sFONT frontType){
+    int pos = ((300-(2*borderOffset)) / (width+1)) * (index%width);
+
+    
+    if(index%width == 0){
+        drawnToX = 0;
+    }
+    
+    if(pos+borderOffset>drawnToX){
+        drawnToX = frontType.Width*strlen(input)+pos;
+        return pos+borderOffset;
+    } else {
+        pos = drawnToX + frontType.Width;
+        drawnToX = frontType.Width*strlen(input)+drawnToX+frontType.Width;
+        return pos;
+    }
+}
+
 int EPD_toDisplay_draw(int position, char *input){
+    bool correctPercent = true;
+    bool firstLine = true;
 
-    //Paint_SetRotate(270);
-    Paint_DrawString_EN(10, 0, "T9/1.12", &Font24, WHITE, BLACK);
-    Paint_DrawString_EN(190, 0, "Montag", &Font24, WHITE, BLACK);
+    if(sizeLines == 1){
+        printf("this!\n");
 
-    if(position == 0){
-        Paint_DrawString_EN(10, 40, "08:00-10:00", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(150, 40, input, &Font16, WHITE, BLACK);
+
+        Paint_DrawString_EN((300-(Font24.Width*strlen(strdup(input))))/2, (400-Font24.Height)/2, input, &Font24, WHITE, BLACK);
+        return 0;
     }
-    if(position == 1){
-        Paint_DrawString_EN(10, 100, "10:00-12:00", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(150, 100, input, &Font16, WHITE, BLACK);
+
+    int xPos = 0;
+    if(position < sizeRows-1){
+        xPos = calcTextPos(position,sizeRows,input,Font24);
+    } else {
+        xPos = calcTextPos(position,sizeRows,input,Font16);
     }
-    if(position == 2){
-        Paint_DrawString_EN(10, 160, "12:00-14:00", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(150, 160, input, &Font16, WHITE, BLACK);
+
+    for(int a = 0; a < sizeRows; a++){
+        if(widthRows[a] > 100){
+            correctPercent = false;
+        }
     }
-    if(position == 3){
-        Paint_DrawString_EN(10, 220, "14:00-16:00", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(150, 220, input, &Font16, WHITE, BLACK);
+
+    printf("init: %s | x = %d | y = %d | len: %d | xPos: %d | posi: %d\n",input,sizeLines,sizeRows,strlen(input),xPos,position);
+    printf("\n");
+
+    if(!correctPercent){
+        printf("yes\n");
+        if(position == 0){
+            Paint_DrawString_EN(xPos, 0, input, &Font24, WHITE, BLACK);
+        }
+        if(position == 1){
+            Paint_DrawString_EN(xPos, 0, input, &Font24, WHITE, BLACK);
+        }
+        if(position == 2){
+            Paint_DrawString_EN(xPos, 0, input, &Font24, WHITE, BLACK);
+        }
+        if(position > 2 && position < 6){
+            Paint_DrawString_EN(xPos, 120, input, &Font16, WHITE, BLACK);
+        }
+        if(position > 5 && position < 9){
+            Paint_DrawString_EN(xPos, 180, input, &Font16, WHITE, BLACK);
+        }
+        if(position > 8 && position < 12){
+            Paint_DrawString_EN(xPos, 240, input, &Font16, WHITE, BLACK);
+        }
     }
-    if(position == 4){
-        Paint_DrawString_EN(10, 280, "16:00-18:00", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(150, 280, input, &Font16, WHITE, BLACK);
-    }
-    if(position == 5){
-        Paint_DrawString_EN(10, 340, "18:00-20:00", &Font16, WHITE, BLACK);
-        Paint_DrawString_EN(150, 340, input, &Font16, WHITE, BLACK);
-    }
-    if(position == 6){
-        printf("test 7\n");
-    }
-    if(position == 7){
-        printf("test 8\n");
-    }
+    
+    
 
     return 0;
 }
@@ -109,8 +133,6 @@ int EPD_toDisplay_flush(){
 
     DEV_Delay_ms(2000);//important, at least 2s
     
-    // close 5V
-    printf("close 5V, Module enters 0 power consumption ...\r\n");
     DEV_Module_Exit();
 
     free(BlackImage);
--- display/examples/toDisplay.c
+++ display/examples/toDisplay.c
@@ -1,6 +1,12 @@
-﻿#include <stdlib.h>     //exit()
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>     //exit()
 #include <signal.h>     //signal()
 #include "EPD_Test.h"   //Examples
+
+int qLines;
+int qRows;
+int percentRow[];
 
 void  Handler(int signo)
 {
@@ -11,21 +17,93 @@ void  Handler(int signo)
     exit(0);
 }
 
-
+/*
+int main(){
+    char str[1024] = "r|5|l|10|c|85\nT9|K.12|Mon\n08:00 -|10:00|SWP\n10:00 -|14:00|Unbenutzt\n14:00 -|16:00|Techno";
+    //char str[50] = "r|5|l|10|c|85\nT9||\n";
+    //printf("Enter a string : ");
+    //gets(str);
+    toDisplay(str);
+}
+*/
 
 int toDisplay(char Text[]){
     signal(SIGINT, Handler);
-    EPD_toDisplay();
-    char *token = strtok(Text, "|");
+    
+
+    char multiCharList[10][50];
+
+    int lineNr = 0;
+    char *line = strtok(Text, "\n");
+    while (line != NULL)
+    {
+        //printf("this %d %s \n",lineNr,line);
+        strcpy(multiCharList[lineNr],strdup(line));
+
+        line = strtok(NULL, "\n");
+        lineNr = lineNr + 1;
+    }
+    qLines = lineNr;
+    getPositionInfo(multiCharList[0]);
+
+    EPD_toDisplay(qLines-1,qRows,percentRow);
+
+    int b = 0;
+    for(int a = 0; a < lineNr-1; a++){
+        b = DisplayFunction(multiCharList[a+1],b*a);
+    }
+    
+    EPD_toDisplay_flush();
+    return 0;
+}
+
+void getPositionInfo(char line[]){
+
+    char mulitList[10][50];
+
+    char *token = strtok(line, "|");
     int i = 0;
     while (token != NULL)
     {
-        char *toPrint;
-        toPrint = token;
-        EPD_toDisplay_draw(i,toPrint);
+        strcpy(mulitList[i],strdup(token));
         token = strtok(NULL, "|");
         i = i + 1;
     }
-    EPD_toDisplay_flush();
-    return 0;
+
+    printf("width: %d \n",i/2);
+    int widthL[i/2];
+    int allignment[i/2];
+
+    percentRow[15];
+    qRows = i/2;
+
+    for(int a = 0; a < i; a++){
+        if(strpbrk("l", mulitList[a]) != 0){
+            allignment[a/2] = 0;
+        } else {
+            if(strpbrk("c", mulitList[a]) != 0){
+                allignment[a/2] = 1;
+            } else {
+                if(strpbrk("r", mulitList[a]) != 0){
+                    allignment[a/2] = 2;
+                } else {
+                    percentRow[a/2] = atoi(mulitList[a]);
+                }
+            }
+        }
+    }
+}
+
+int DisplayFunction(char line[], int offset){
+
+    char *token = strtok(line, "|");
+    int i = 0;
+    while (token != NULL)
+    {
+        EPD_toDisplay_draw(i+offset,token);
+        token = strtok(NULL, "|");
+        i = i + 1;
+    }
+
+    return i;
 }
